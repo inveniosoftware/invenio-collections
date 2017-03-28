ed27536d1cce4f1cd96cc59de886785270ea7d7a
hahirvon <hjhsalo@gmail.com>
2017-03-28 20:42:27 +0200

    tests: simplify doctest execution
    * doctests are now executed with pytest instead of sphinx.
      ( closes #78 )
    
    * added missing copyright information
    
    Signed-off-by: Harri Hirvonsalo <harri.hirvonsalo@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015, 2016, 2017 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -76,7 +76,10 @@ author = u'CERN'
 
 # Get the version string. Cannot be done with import!
 g = {}
-with open(os.path.join('..', 'invenio_collections', 'version.py'), 'rt') as fp:
+with open(os.path.join(os.path.dirname(__file__),
+                       '..',
+                       'invenio_collections',
+                       'version.py'), 'rt') as fp:
     exec(fp.read(), g)
     version = g['__version__']
 
