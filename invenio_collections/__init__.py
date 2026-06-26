3d3f028ad08229fd92299ef78683ea9ef4ae5427
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2026-06-26 11:40:22 +0200

    Release maint-v9.0.1

--- invenio_collections/__init__.py
+++ invenio_collections/__init__.py
@@ -1,12 +1,13 @@
 # SPDX-FileCopyrightText: 2015-2025 CERN.
 # SPDX-FileCopyrightText: 2025 Ubiquity Press.
 # SPDX-FileCopyrightText: 2025-2026 Graz University of Technology.
+# SPDX-FileCopyrightText: 2026 KTH Royal Institute of Technology.
 # SPDX-License-Identifier: MIT
 
 """Invenio module for organizing metadata into collections."""
 
 from .ext import InvenioCollections
 
-__version__ = "9.0.0"
+__version__ = "9.0.1"
 
 __all__ = ("__version__", "InvenioCollections")
